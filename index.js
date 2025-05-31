d2a04c67eac6e707c6e194cb82ac6b7c912f6ee2
yousif <138237408+yousifj129@users.noreply.github.com>
2025-05-31 14:49:28 +0300

    aaa

--- index.js
+++ index.js
@@ -3,10 +3,22 @@
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const output = document.getElementById("output");
+const container = document.getElementById("container");
+
 
 button.addEventListener("click", () => {
-    const date = input.value ;
-    console.log(date)
+    const date = input.value.trim();
+
+    // Check if the input matches the Month/day format (e.g., 5/31 or 12/1)
+    const datePattern = /^(1[0-2]|[1-9])\/(3[01]|[12][0-9]|[1-9])$/;
+
+    if (!datePattern.test(date)) {
+        output.innerHTML = "Please enter a valid date in Month/day format (e.g., 5/31)";
+        return;
+    }
+
+    // Input is valid
+    console.log(date);
     output.innerHTML = randomHistoricFacts(date);
 });
 
@@ -27,9 +39,11 @@ function randomHistoricFacts(date) {
     const rand = Math.floor(Math.random() * data['data']['Events'].length);
     const randomFact = data['data']['Events'][rand]['text'];
     const year = data['data']['Events'][rand]['year'];
+    const inHtml = data['data']['Events'][rand]['html'];
     fact = randomFact;
     console.log(fact + "    " + year);
-    output.innerText = fact;
+    output.innerText = fact + " - " + year;
+    container.innerHTML = inHtml;
   })
   .catch(error => {
     console.error('Error:', error);
