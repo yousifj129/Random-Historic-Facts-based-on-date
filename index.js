b27ef0a3a2a60f0ff6d069ba0afd8ad658a5cc18
yousif <138237408+yousifj129@users.noreply.github.com>
2025-05-31 14:39:39 +0300

    aaa

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const input = document.getElementById("input");
 const output = document.getElementById("output");
 
 button.addEventListener("click", () => {
-    const date = input.value;
+    const date = input.value ;
+    console.log(date)
     output.innerHTML = randomHistoricFacts(date);
 });
 
 function randomHistoricFacts(date) {
     const apiUrl = 'https://history.muffinlabs.com/date/'+ date;
 
-    const fact = "";
+    let fact = "";
     // Make a GET request
   fetch(apiUrl)
   .then(response => {
@@ -23,8 +24,12 @@ function randomHistoricFacts(date) {
   })
   .then(data => {
     //choose a random fact from the data
-    const randomFact = data['data']['Events'][Math.floor(Math.random() * data.length)];
+    const rand = Math.floor(Math.random() * data['data']['Events'].length);
+    const randomFact = data['data']['Events'][rand]['text'];
+    const year = data['data']['Events'][rand]['year'];
     fact = randomFact;
+    console.log(fact + "    " + year);
+    output.innerText = fact;
   })
   .catch(error => {
     console.error('Error:', error);
